e97e39b89ddf3ce103b48ae5b3a6865c42842c49
Dez <desmondcoulton@gmail.com>
2022-04-15 13:19:23 +1000

    april 15th

--- ldapmanager.py
+++ ldapmanager.py
@@ -2,25 +2,28 @@
 
 import os
 
-menu_options={
- 1:'Group1',
- 2:'Group2',
- 3:'Back',
+menu_options = {
+    1: 'Group1',
+    2: 'Group2',
+    3: 'Back',
 }
-main_options={
- 1:'Create User',
- 2:'Delete User',
- 3:'Change password',
- 4:'Exit',
+main_options = {
+    1: 'Create User',
+    2: 'Delete User',
+    3: 'Change password',
+    4: 'Exit',
 }
+
 
 def print_main():
     for key in main_options.keys():
-        print ( key, '--',main_options[key])
+        print(key, '--', main_options[key])
+
 
 def print_menu():
     for key in menu_options.keys():
-      print (key, '--',menu_options[key])
+        print(key, '--', menu_options[key])
+
 
 def main_menu():
     print_main()
@@ -28,7 +31,7 @@ def main_menu():
     try:
         option = int(input('Enter your choice: '))
     except:
-        print:('Wrong input. Please enter a number...')
+        print: ('Wrong input. Please enter a number...')
     if option == 1:
         create_user()
     if option == 2:
@@ -39,30 +42,34 @@ def main_menu():
         print('exiting')
     exit()
 
+
 def gid_menu():
     print_menu()
     option = ''
     try:
-       option = int(input('Enter your choice: '))
+        option = int(input('Enter your choice: '))
     except:
-       print:('Wrong input. Please enter a number... ')
+        print: ('Wrong input. Please enter a number... ')
     if option == 1:
-       return 2000
+        return 2000
     if option == 2:
-       return 3000
+        return 3000
     if option == 3:
-       main_menu()
+        main_menu()
+
 
 def finduid(gid):
-    clidata = os.popen("ldapsearch -x -b 'dc=dezmeana,dc=local,dc=au' | grep -B 1 %s | grep uid" % gid).read()
+    clidata = os.popen(
+        "ldapsearch -x -b 'dc=dezmeana,dc=local,dc=au' | grep -B 1 %s | grep uid" % gid).read()
     data = [int(i) for i in clidata.split() if i.isdigit()]
-    newuid= (max(data))
-    intrimuid = newuid ++ 1
+    newuid = (max(data))
+    intrimuid = newuid + + 1
     if intrimuid in data:
-        uid = intrimuid ++ 1
+        uid = intrimuid + + 1
         return uid
     else:
         return intrimuid
+
 
 def create_user():
     usr = input("Enter username: ")
@@ -70,7 +77,7 @@ def create_user():
     lname = input("Enter lastname: ")
     gid = gid_menu()
     uid = finduid(gid)
-    #adding user varibles to the ldap user form.
+    # adding user varibles to the ldap user form.
     ldap = open("ldap.txt", 'r')
     data = ldap.read()
     data = data.replace('{usr}', str(usr))
@@ -80,25 +87,29 @@ def create_user():
     data = data.replace('{uid}', str(uid))
     ldap.close()
 
-    #writing data to the ldif in the correct folder.
+    # writing data to the ldif in the correct folder.
     ldap_form = open("/root/%s.ldif" % usr, "wt")
     ldap_form.write(data)
     ldap.close()
 
-    #display data written to be visually checked before proceeding.
+    # display data written to be visually checked before proceeding.
     print(data)
 
-    #Binding new accout to ldap.
+    # Binding new accout to ldap.
     os.system('sudo ldapadd -x -W -D "cn=admin,dc=dezmeana,dc=local,dc=au" -f /root/%s.ldif' % usr)
     os.system('sudo ldappasswd -S -W -D "cn=admin,dc=dezmeana,dc=local,dc=au" -x "uid=%s,ou=people,dc=dezmeana,dc=local,dc=au"' % usr)
     main_menu()
 
+
 def delete_user():
     main_menu()
+
 
 def chng_pswd():
     usr = input("Enter username: ")
     os.system('ldappasswd -S -W -D "cn=admin,dc=dezmeana,dc=local,dc=au" -x "uid=%s,ou=people,dc=dezmeana,dc=local,dc=au"' % usr)
     main_menu()
 
+
+if __name__ == '__main__':
 main_menu()
